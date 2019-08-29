8078df672756cf11b5513d595792f9e6f004087a
Данил Ченцов <danil090598@gmail.com>
2019-08-29 19:48:26 +0300

    navbar

--- stargen-todo/src/components/Navbar.vue
+++ stargen-todo/src/components/Navbar.vue
@@ -1,21 +1,39 @@
 <template lang="pug">
-    <div>
-        v-toolbar(flat)
+    nav
+        v-toolbar(flat) 
+            v-app-bar-nav-icon.grey--text(@click='drawer = !drawer')
             v-toolbar-title.text-uppercase.grey--text
-                h2.font-weight-light StaringGenie TODO
-            v-spacer    
-            v-btn(color="grey" text)
-                span SignOut
+            span.font-weight-light StaringGenie TODO
+            v-spacer
+            v-btn(text color='grey')
+                span Sign Out
                 v-icon(right) exit_to_app
-
-                    
-
+        v-navigation-drawer(app v-model='drawer')
+            v-list(
+            :rounded="true"
+            )       
+                    v-app-bar-nav-icon.grey--text(@click='drawer = !drawer')
+                    v-list-item(v-for='(item, i) in items' :key='i' router :to= "item.route")
+                        v-list-item-icon
+                            v-icon(v-text='item.icon')
+                        v-list-item-content
+                            v-list-item-title(v-text='item.text')
             
-    </div>
+
 </template>
 
 <script>
 export default {
-    
+    data() {
+        return {
+            drawer: false,
+            items: [
+                {icon: 'dashboard', text: 'Dashboard', route: '/'},
+                {icon: 'folder', text: 'My Projects', route: '/projects'},
+                {icon: 'person', text: 'Team', route: '/team'}
+            ]
+        
+        }
+    },
 }
 </script>
